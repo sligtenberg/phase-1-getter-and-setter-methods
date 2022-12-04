b32ad356c3807e2501a4f3430ab958863ec9d687
stephen ligtenberg <bountyhunter24@gmail.com>
2022-12-04 03:00:06 -0500

    commit tree fc249e0982af2684c20816dfa20c65a76861d548

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Add your Circle class here
+
+class Circle {
+    constructor(radius) {
+        this.radius = radius
+    }
+
+    get diameter() {
+        return this.radius * 2
+    }
+
+    get circumference() {
+        return 2 * Math.PI * this.radius
+    }
+
+    get area() {
+        return Math.PI * this.radius * this.radius
+    }
+
+    set diameter(diameter) {
+        this.radius = 0.5 * diameter
+    }
+
+    set circumference(circumference) {
+        this.radius = circumference / (2 * Math.PI)
+    }
+
+    set area(area) {
+        this.radius = Math.sqrt(area / Math.PI)
+    }
+}
+
+const roundGuy = new Circle(3)
+
+console.log(roundGuy)
+console.log("radius: ", roundGuy.radius)
+console.log("diameter: ", roundGuy.diameter)
+console.log("Circumference: ", roundGuy.circumference)
+console.log("Area: ", roundGuy.area)
+
+roundGuy.diameter = 2
+
+console.log(roundGuy)
+console.log("radius: ", roundGuy.radius)
+console.log("diameter: ", roundGuy.diameter)
+console.log("Circumference: ", roundGuy.circumference)
+console.log("Area: ", roundGuy.area)
